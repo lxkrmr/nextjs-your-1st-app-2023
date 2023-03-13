de2c6f0e54caffd888ad897dad9d55d857584f50
Alexander Kramer <lxkrmr@icloud.com>
2023-03-13 12:02:42 +0100

    ✨ add external script

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Head from "next/head";
+import Script from "next/script";
 
 function FirstPost() {
   return (
@@ -7,6 +8,12 @@ function FirstPost() {
       <Head>
         <title>First Post</title>
       </Head>
+      <Script
+        src="https://code.jquery.com/jquery-3.6.4.js"
+        strategy="lazyOnload"
+        onLoad={() => console.log("script loaded correctly")}
+      />
+
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to to home</Link>
