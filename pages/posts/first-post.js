393a3567797335045425c7735584e5fe0c14a756
Alexander Kramer <lxkrmr@icloud.com>
2023-03-13 12:27:26 +0100

    ✨ introduce layout component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import Head from "next/head";
 import Script from "next/script";
+import Layout from "../../components/layout";
 
 function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
@@ -18,7 +19,7 @@ function FirstPost() {
       <h2>
         <Link href="/">Back to to home</Link>
       </h2>
-    </>
+    </Layout>
   );
 }
 
