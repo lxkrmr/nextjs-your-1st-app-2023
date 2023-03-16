3eae92a023f93426cadce8363ee5b8ecbd47b0f1
Alexander Kramer <lxkrmr@icloud.com>
2023-03-16 14:02:11 +0100

    ✨ implement getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
 
-function Home() {
+function Home({ allPostData }) {
   return (
     <Layout home>
       <Head>
@@ -15,8 +16,30 @@ function Home() {
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostData.map(({ id, date, title }) => {
+            return (
+              <li className={utilStyles.listItem} key={id}>
+                {title} <br /> {id} <br /> {date}
+              </li>
+            );
+          })}
+        </ul>
+      </section>
     </Layout>
   );
 }
 
+async function getStaticProps() {
+  const allPostData = getSortedPostsData();
+  return {
+    props: { allPostData },
+  };
+}
+
 export default Home;
+
+export { getStaticProps };
